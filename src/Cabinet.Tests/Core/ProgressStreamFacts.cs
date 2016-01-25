e5b4ffe41cf5dc76a04c5468f382b9584d3538be
John McKim <johncmckim@gmail.com>
2016-01-25 21:28:36 +1000

    Add more progress stream tests

--- src/Cabinet.Tests/Core/ProgressStreamFacts.cs
+++ src/Cabinet.Tests/Core/ProgressStreamFacts.cs
@@ -21,7 +21,6 @@ namespace Cabinet.Tests.Core {
         [InlineData(true), InlineData(false)]
         public void Inner_Stream_Calls_CanRead(bool canRead) {
             var mockStream = new Mock<Stream>();
-            var mockProgress = new Mock<IProgress<WriteProgress>>();
             var progressStream = new ProgressStream(mockStream.Object, null, null);
 
             mockStream.SetupGet(s => s.CanRead).Returns(canRead);
@@ -37,7 +36,6 @@ namespace Cabinet.Tests.Core {
         [InlineData(true), InlineData(false)]
         public void Inner_Stream_Calls_CanWrite(bool canWrite) {
             var mockStream = new Mock<Stream>();
-            var mockProgress = new Mock<IProgress<WriteProgress>>();
             var progressStream = new ProgressStream(mockStream.Object, null, null);
 
             mockStream.SetupGet(s => s.CanWrite).Returns(canWrite);
@@ -53,7 +51,6 @@ namespace Cabinet.Tests.Core {
         [InlineData(true), InlineData(false)]
         public void Inner_Stream_Calls_CanSeek(bool canSeek) {
             var mockStream = new Mock<Stream>();
-            var mockProgress = new Mock<IProgress<WriteProgress>>();
             var progressStream = new ProgressStream(mockStream.Object, null, null);
 
             mockStream.SetupGet(s => s.CanWrite).Returns(canSeek);
@@ -63,6 +60,113 @@ namespace Cabinet.Tests.Core {
             mockStream.Verify(s => s.CanWrite, Times.Once);
 
             Assert.Equal(canSeek, actualCanSeek);
+        }
+
+        [Theory]
+        [InlineData(0), InlineData(10)]
+        public void Inner_Stream_Calls_Length(long length) {
+            var mockStream = new Mock<Stream>();
+            var progressStream = new ProgressStream(mockStream.Object, null, null);
+
+            mockStream.SetupGet(s => s.Length).Returns(length);
+
+            long actualLength = progressStream.Length;
+
+            mockStream.Verify(s => s.Length, Times.Once);
+
+            Assert.Equal(length, actualLength);
+        }
+
+        [Theory]
+        [InlineData(0), InlineData(10)]
+        public void Inner_Stream_Calls_Position(long position) {
+            var mockStream = new Mock<Stream>();
+            var progressStream = new ProgressStream(mockStream.Object, null, null);
+
+            mockStream.SetupGet(s => s.Position).Returns(position);
+
+            long actualPosition = progressStream.Position;
+
+            mockStream.Verify(s => s.Position, Times.Once);
+
+            Assert.Equal(position, actualPosition);
+        }
+
+        [Fact]
+        public void Inner_Stream_Calls_Flush() {
+            var mockStream = new Mock<Stream>();
+            var progressStream = new ProgressStream(mockStream.Object, null, null);
+
+            progressStream.Flush();
+
+            mockStream.Verify(s => s.Flush(), Times.Once);
+        }
+
+        [Fact]
+        public void Inner_Stream_Calls_Read() {
+            var mockStream = new Mock<Stream>();
+            var progressStream = new ProgressStream(mockStream.Object, null, null);
+
+            byte[] buffer = new byte[0];
+            int offset = 0;
+            int count = 0;
+
+            progressStream.Read(buffer, offset, count);
+
+            mockStream.Verify(s => s.Read(buffer, offset, count), Times.Once);
+        }
+
+        [Fact]
+        public void Inner_Stream_Calls_Seek() {
+            var mockStream = new Mock<Stream>();
+            var progressStream = new ProgressStream(mockStream.Object, null, null);
+
+            long offset = 0;
+            SeekOrigin origin = SeekOrigin.Begin;
+
+            progressStream.Seek(offset, origin);
+
+            mockStream.Verify(s => s.Seek(offset, origin), Times.Once);
+        }
+
+        [Fact]
+        public void Inner_Stream_Calls_SetLength() {
+            var mockStream = new Mock<Stream>();
+            var progressStream = new ProgressStream(mockStream.Object, null, null);
+
+            long length = 0;
+
+            progressStream.SetLength(length);
+
+            mockStream.Verify(s => s.SetLength(length), Times.Once);
+        }
+
+        [Fact]
+        public void Inner_Stream_Calls_Write() {
+            var mockStream = new Mock<Stream>();
+            var mockProgress = new Mock<IProgress<WriteProgress>>();
+            var progressStream = new ProgressStream(mockStream.Object, null, mockProgress.Object);
+
+            byte[] buffer = new byte[0];
+            int offset = 0;
+            int count = 0;
+
+            mockProgress.Setup(p => p.Report(It.IsAny<WriteProgress>()));
+
+            progressStream.Write(buffer, offset, count);
+
+            mockProgress.Verify(p => p.Report(It.IsAny<WriteProgress>()), Times.Once);
+            mockStream.Verify(s => s.Write(buffer, offset, count), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(true), InlineData(false)]
+        public void Inner_Stream_Calls_Dispose(bool disposeStream) {
+            var mockStream = new Mock<Stream>();
+            var mockProgress = new Mock<IProgress<WriteProgress>>();
+            var progressStream = new ProgressStream(mockStream.Object, null, mockProgress.Object, disposeStream);
+
+            progressStream.Dispose();
         }
     }
 }
