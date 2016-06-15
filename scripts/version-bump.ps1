708d60f9e1eff1a94b4226bd2140ec5772dde071
John McKim <johncmckim@gmail.com>
2016-06-15 16:18:47 +1000

    Fix version bump script

--- scripts/version-bump.ps1
+++ scripts/version-bump.ps1
@@ -12,7 +12,9 @@ if ($env:APPVEYOR_REPO_TAG_NAME) {
 	$version = $env:APPVEYOR_REPO_TAG_NAME
 }
 
-$buildFolder = '.'
+$buildFolder = (Get-Item $PSScriptRoot).Parent.FullName
+
+Write-Host "Working from build folder: $buildFolder"
 
 if($env:APPVEYOR_BUILD_FOLDER) {
 	$buildFolder = $env:APPVEYOR_BUILD_FOLDER
@@ -24,7 +26,9 @@ Write-Host "Bumping version to $version"
 
 $projectFilePaths | 
   Foreach {
+	Write-Host "Updating $_ to $version"
+
 	(gc -Path $_) `
-	  -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$version" |
+	  -replace "(?<=`"version`":\s`")[.\w\-\*]*(?=`",)", "$version" |
 	  sc -Path $_ -Encoding UTF8
   }
