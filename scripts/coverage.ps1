4502ede35f36b08c6c0a336c75a148e986c1e3b7
John McKim <johncmckim@gmail.com>
2016-06-16 14:55:45 +1000

    Add coverage script

---
+++ scripts/coverage.ps1
@@ -0,0 +1,35 @@
+
+$PROJECT_ROOT=(Split-Path $PSScriptRoot -Parent)
+
+$PROJECTS_FILTER='Cabinet*'
+$TEST_PROJECTS_FILTER='Cabinet.Tests'
+
+$OPEN_COVER_VERSION='4.6.519'
+$OPEN_COVER_PATH=(Join-Path $env:USERPROFILE ".nuget\packages\OpenCover\$OPEN_COVER_VERSION\tools\OpenCover.Console.exe")
+
+$COVERALLS_VERSION='1.3.4'
+$COVERALLS_PATH=(Join-Path $env:USERPROFILE ".nuget\packages\coveralls.io\$COVERALLS_VERSION\tools\coveralls.net.exe")
+
+$COVERAGE_COMMAND=$OPEN_COVER_PATH + 
+	' -register:user' +
+	' -target:"C:\Program Files\dotnet\dotnet.exe"' +
+	" -filter:`"+[$PROJECTS_FILTER]* -[$TEST_PROJECTS_FILTER]*`""
+
+$TARGET_ARGS="test $PROJECT_ROOT\test\Cabinet.Tests\"
+
+if($env:APPVEYOR -eq $true) {
+	$TARGET_ARGS+=''
+}
+
+$COVERAGE_COMMAND+=" -targetargs:`"$TARGET_ARGS`""
+
+if($env:CI -eq $true) {
+	$COVERAGE_COMMAND+=' -output:coverage.xml'
+}
+
+Write-Host "Executing $COVERAGE_COMMAND"
+iex $COVERAGE_COMMAND
+
+if($env:CI -eq $true) {
+	& $COVERALLS_PATH --opencover --repo-token $env:COVERALLS_REPO_TOKEN --full-sources coverage.xml
+}
