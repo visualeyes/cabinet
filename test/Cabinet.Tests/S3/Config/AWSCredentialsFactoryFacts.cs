48e4642f2de9c3da44d06b609313ad0dc280a21e
John McKim <johncmckim@gmail.com>
2016-06-16 15:39:03 +1000

    Increase coverage

---
+++ test/Cabinet.Tests/S3/Config/AWSCredentialsFactoryFacts.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.Runtime;
+using Cabinet.S3.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cabinet.Tests.S3.Config {
+    public class AWSCredentialsFactoryFacts {
+        private readonly AWSCredentialsFactory factory;
+
+        public AWSCredentialsFactoryFacts() {
+            this.factory = new AWSCredentialsFactory();
+        }
+
+        [Theory]
+        [InlineData("accessKey", "secretKey")]
+        public void Basic_Credentials(string accessKey, string secretKey) {
+            var credentials = factory.GetBasicCredentials(accessKey, secretKey);
+            var basicCredentials = credentials as BasicAWSCredentials;
+            
+            Assert.NotNull(basicCredentials);
+
+            var immutableCredentials = basicCredentials.GetCredentials();
+            Assert.Equal(accessKey, immutableCredentials.AccessKey);
+            Assert.Equal(secretKey, immutableCredentials.SecretKey);
+        }
+
+        [Theory]
+        [InlineData("accessKey", "")]
+        [InlineData("accessKey", null)]
+        [InlineData("", "secretKey")]
+        [InlineData(null, "secretKey")]
+        public void Basic_Credentials_Null_Throws(string accessKey, string secretKey) {
+            Assert.Throws<ArgumentNullException>(() => factory.GetBasicCredentials(accessKey, secretKey));
+        }
+    }
+}
