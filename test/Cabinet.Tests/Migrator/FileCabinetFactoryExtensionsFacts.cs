5d5c601440df11016c90d1e6e0907c532a52ef7d
John McKim <johncmckim@gmail.com>
2016-06-29 13:46:22 +1000

    Add unit test

--- test/Cabinet.Tests/Migrator/FileCabinetFactoryExtensionsFacts.cs
+++ test/Cabinet.Tests/Migrator/FileCabinetFactoryExtensionsFacts.cs
@@ -1,6 +1,7 @@
 ﻿using Cabinet.Core;
 using Cabinet.Migrator;
 using Cabinet.Migrator.Migration;
+using Cabinet.Migrator.Replication;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,19 @@ namespace Cabinet.Tests.Migrator {
     public class FileCabinetFactoryExtensionsFacts {
 
         [Fact]
-        public void Registers_Provider() {
+        public void Registers_Migration_Provider() {
             var mockFactory = new Mock<IFileCabinetFactory>();
             
             mockFactory.Object.RegisterMigratorProvider();
             mockFactory.Verify(f => f.RegisterProvider(It.IsNotNull<MigrationStorageProvider>()), Times.Once);
+        }
+
+        [Fact]
+        public void Registers_Replicated_Provider() {
+            var mockFactory = new Mock<IFileCabinetFactory>();
+
+            mockFactory.Object.RegisterReplicationProvider();
+            mockFactory.Verify(f => f.RegisterProvider(It.IsNotNull<ReplicatedStorageProvider>()), Times.Once);
         }
     }
 }
