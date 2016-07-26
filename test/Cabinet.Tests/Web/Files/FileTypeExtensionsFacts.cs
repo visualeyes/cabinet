cb84ef8ba93da13ef58d5c53f936ef5be85d0363
John McKim <johncmckim@gmail.com>
2016-07-26 11:55:37 +1000

    Fix case sensitive comparison

---
+++ test/Cabinet.Tests/Web/Files/FileTypeExtensionsFacts.cs
@@ -0,0 +1,21 @@
+﻿using Cabinet.Web.Files;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cabinet.Tests.Web.Files {
+    public class FileTypeExtensionsFacts {
+        [Theory]
+        [InlineData("jpg", true), InlineData("jPg", true), InlineData("JPG", true)]
+        [InlineData("exe", false), InlineData("eXe", false), InlineData("EXE", false)]
+        public void Case_Insenstive_FileType_Check(string ext, bool allowed) {
+            var mockFileType = new Mock<IFileType>();
+            mockFileType.SetupGet(ft => ft.Extensions).Returns(new string[] { "jpg" });
+
+            Assert.Equal(allowed, mockFileType.Object.IsValidExtension(ext));
+        }
+    }
+}
